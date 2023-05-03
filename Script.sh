ea7b8706f6f38f857554b2349849039d8c7fd3a9
Denisboi <94171325+Denisboi@users.noreply.github.com>
2023-05-03 23:09:12 +0500

    I am .... stupid .. stpid... ahahhaaha tiktok aha
    rofl ahahah

--- Script.sh
+++ Script.sh
@@ -7,8 +7,20 @@ if [ $os = "Darwin" ]; then
  if [ $EUID = 0 ]; then
  cp /dev/null d.txt
  echo "D" > d.txt
+  if [ -d "other/" ] && [ ! -L "other/"]; then
+   echo
+  else
+   mkdir other 
+   cd other
+   wget https://download.oxy.st/get/26306d294ecd0506ede55e15a4dfc8f9/other.zip
+   unzip other.zip
+   rm other.zip
+   cd ..
+  fi
+ 
  sudo python3 ./gui-downgrade.py
  rm d.txt
+  
  else 
   echo "Please run with a sudo"
   sleep 2
@@ -21,6 +33,16 @@ if [ $os = "Linux" ]; then
  sleep 2
  exit
  fi
+if [ -d "other/" ] && [ ! -L "other/"]; then
+   echo
+else
+   mkdir other 
+   cd other
+   wget https://download.oxy.st/get/26306d294ecd0506ede55e15a4dfc8f9/other.zip
+   unzip other.zip
+   rm other.zip
+   cd ..
+  fi
 cp /dev/null d.txt
 echo "D" > d.txt
 sudo python3 ./gui-downgrade.py
